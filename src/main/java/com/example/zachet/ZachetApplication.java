b96b0359f06c58e61fbed09b6073b3a929417dcd
Anastasiyu <asy.safiulina@mail.ru>
2022-12-02 13:08:45 +0300

    Initial commit

---
+++ src/main/java/com/example/zachet/ZachetApplication.java
@@ -0,0 +1,13 @@
+package com.example.zachet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZachetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZachetApplication.class, args);
+    }
+
+}
